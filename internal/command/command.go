0c780a7cf2faadbe7ddca1f8d7e4803a57746d12
quockhanhcao <quockhanhcao99@gmail.com>
2025-07-09 23:39:17 +0700

    Add more state to the state machine for parsing the request

--- internal/command/command.go
+++ internal/command/command.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"strconv"
+	"strings"
 )
 
 type Command struct {
@@ -19,6 +20,8 @@ type CommandParserState int
 
 const (
 	commandParserStateInitialize CommandParserState = iota
+	commandParserStateArgSize
+	commandParserStateArgBody
 	commandParserStateDone
 )
 
@@ -43,8 +46,17 @@ func CommandFromReader(reader io.Reader) (*Command, error) {
 		readBytes, err := reader.Read(buf[readToIndex:])
 		if err != nil {
 			if err == io.EOF {
-				// think about the state of the command here
-				// what is the condition for stop parsing?
+				if cmd.state == commandParserStateArgBody {
+					return nil, fmt.Errorf("incomplete command data, need more data to parse the command parameters")
+				} else if cmd.state == commandParserStateArgSize {
+					if cmd.NumberOfArgs == len(cmd.Args) {
+						// cmd is fully parsed
+						cmd.Name = strings.ToLower(cmd.Args[0])
+						cmd.state = commandParserStateDone
+						return cmd, nil
+					}
+					return nil, fmt.Errorf("incomplete command data, need more params")
+				}
 				break
 			}
 			return nil, err
@@ -55,7 +67,7 @@ func CommandFromReader(reader io.Reader) (*Command, error) {
 			return nil, err
 		}
 		// remove the parsed bytes from the buffer
-		copy(buf, buf[parsedBytes:])
+		copy(buf, buf[parsedBytes:readToIndex])
 		readToIndex -= parsedBytes
 	}
 	return cmd, nil
@@ -73,9 +85,30 @@ func (c *Command) parse(data []byte) (int, error) {
 			return 0, nil
 		}
 		c.NumberOfArgs = numberOfArgs
-		c.Args = make([]string, numberOfArgs)
-		c.state = commandParserStateDone
+		c.state = commandParserStateArgSize
 		return parsedBytes, nil
+	case commandParserStateArgSize:
+		parsedBytes, err := c.parseCommandArgSize(data)
+		if err != nil {
+			return 0, err
+		}
+		if parsedBytes == 0 {
+			// need more data
+			return 0, nil
+		}
+		return parsedBytes, nil
+	case commandParserStateArgBody:
+		parsedBytes, err := c.parseCommandArgBody(data)
+		if err != nil {
+			return 0, err
+		}
+		if parsedBytes == 0 {
+			// need more data
+			return 0, nil
+		}
+		return parsedBytes, err
+	case commandParserStateDone:
+		return 0, fmt.Errorf("command is already parsed")
 	default:
 		return 0, fmt.Errorf("unknown command parser state: %d", c.state)
 	}
@@ -96,4 +129,49 @@ func getNumberOfArgs(data []byte) (numberOfArgs, parsedBytes int, err error) {
 		return 0, 0, fmt.Errorf("failed to parse number of params: %w", err)
 	}
 	return numberOfArgs, idx + len(CRLF), nil
+}
+
+// return number of bytes parsed and error if any
+func (c *Command) parseCommandArgSize(data []byte) (int, error) {
+	idx := bytes.Index(data, []byte(CRLF))
+	if idx == -1 {
+		// need more data
+		return 0, nil
+	}
+	part := data[:idx]
+	if part[0] != '$' {
+		return 0, fmt.Errorf("wrong format of command's param size line")
+	}
+	size, err := strconv.Atoi(string(part[1:]))
+	if err != nil {
+		return 0, fmt.Errorf("failed to parse command's param size: %w", err)
+	}
+	c.currentArgSize = size
+	// if parsed successfully
+	c.state = commandParserStateArgBody
+	return idx + 2, nil
+}
+
+func (c *Command) parseCommandArgBody(data []byte) (int, error) {
+	idx := bytes.Index(data, []byte(CRLF))
+	if idx == -1 {
+		// need more data
+		return 0, nil
+	}
+	param := string(data[:idx])
+	if len(param) != c.currentArgSize {
+		return 0, fmt.Errorf("command's param body size mismatch: expected %d, got %d", c.currentArgSize, len(param))
+	}
+	c.Args = append(c.Args, param)
+	if len(c.Args) == c.NumberOfArgs {
+		// not really ideal
+		// TODO: refactor this state machine
+		c.Name = strings.ToLower(c.Args[0])
+		c.state = commandParserStateDone
+	} else {
+		// if parsed successfully
+		c.state = commandParserStateArgSize
+	}
+	// c.state = commandParserStateParamSize
+	return idx + 2, nil
 }
